1a919fd93a123135d7821e2bae7f3b690d528318
Alexander <kryvondenchenkov@gmail.com>
2023-03-16 00:58:45 +0200

    My first commits

---
+++ ruby/test-2.rb
@@ -0,0 +1,6 @@
+puts "Hi dench"
+puts "Введи желаемое число в км"
+user_input_in_km = gets.to_f
+result_in_mill = user_input_in_km / 1.6
+puts "Получай своё значение: #{result_in_mill}миль"
+puts "До встречи"
