953508fc9c010238f90bc273e7a78960fd442fcc
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-04-20 23:41:51 +0300

    add symbol

--- ruby/test-1.rb
+++ ruby/test-1.rb
@@ -1,6 +1,6 @@
 
 puts "Hi dench!!!"
-puts "Введи желаемое число в милях"
+puts "Введи желаемое число в милях."
 user_input_in_miles = gets.to_f
 result_in_km = user_input_in_miles*1.6
 puts "Получай своё расстояние в километрах: #{result_in_km}км"
