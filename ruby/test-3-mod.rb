e50f4577f5007ebfca78032f84670a7d143be694
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-05-02 01:57:08 +0300

    homework

---
+++ ruby/test-3-mod.rb
@@ -0,0 +1,27 @@
+puts "Введіть свій вік:"
+age = gets.to_i
+
+puts "Вкажіть свою краіну: 
+1 - якщо Ви з США або Японії
+2 - якщо ні"
+country = gets.strip
+
+if country == 1.to_s
+    country = "Японія"
+elsif country == 2.to_s
+    country = "Інша країна"
+else country !=1 || country !=2
+    puts "Не вірне значенняю. Ведіть 1 або 2"
+    # Нужно как-то вернуться к строке 7
+end
+
+# puts country.inspect
+
+if (country == "Японія" || country == "США") && age>=21
+    puts "Вітаю, переходь до наступного кроку"
+elsif age >= 18
+    puts "Доступ відкрит, але не для Японії та США"    
+else
+    puts "Перепрошую цей сайт не для вас;("
+end
+
