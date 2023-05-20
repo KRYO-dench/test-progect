d1eb667e6c52c1a8f035e054803199009f3c743e
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-05-20 23:41:27 +0300

    renaming files

---
+++ ruby/guesser (elsif+loop).rb
@@ -0,0 +1,15 @@
+random_number = (rand *10).to_i + 1
+
+puts "Я загадав число от 1 до 10. Спробуй здогадатись..."
+
+# puts random_number
+
+guess = gets.to_i
+
+if guess == random_number
+    puts "Чудово, ти вгадав!!!"
+elsif guess < random_number
+    puts "Моє число більше, ніж твій здогад. Спробуй ще;)"
+else guess > random_number
+    puts "Моє число меньше, ніж твій здогад. Спробуй ще;)"
+end
