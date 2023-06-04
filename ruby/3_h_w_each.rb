8c8e2933bf5e6fb94bed41474407877decb6875d
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-06-05 02:44:52 +0300

    adjustment

--- ruby/3_h_w_each.rb
+++ ruby/3_h_w_each.rb
@@ -1,8 +1,18 @@
 # degreeses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] незручно створювати
+
 # або керованний масив
 # degreeses = Array.new(10) {|array_element| array_element + 1}
+
 # або упорядкований масив
 degreeses = (1..10).to_a
 degreeses.each do |degree|
     puts "#{degree} --> #{degree ** 2}"
 end
+
+puts degreeses.inspect
+
+#перебір диапазону
+# (1..10).each do |degree|
+#     puts "#{degree} --> #{degree ** 2}"
+# end
+
