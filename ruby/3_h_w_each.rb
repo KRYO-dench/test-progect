34dd605cd7d6bebe1ba6cb6db089015a8bb34273
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-06-05 01:48:10 +0300

    optimization_of_creation

--- ruby/3_h_w_each.rb
+++ ruby/3_h_w_each.rb
@@ -1,4 +1,8 @@
-degreeses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# degreeses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] незручно створювати
+# або керованний масив
+# degreeses = Array.new(10) {|array_element| array_element + 1}
+# або упорядкований масив
+degreeses = (1..10).to_a
 degreeses.each do |degree|
     puts (degree * degree)
 end
