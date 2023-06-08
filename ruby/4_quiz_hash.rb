205b57f096c99bce2e7abf71ee12b6527dbe2ca1
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-06-09 02:11:11 +0300

    with_hash

---
+++ ruby/4_quiz_hash.rb
@@ -0,0 +1,22 @@
+date = {
+  Украина: "Київ",
+  Канада: "Оттава",
+  Великобритания: "Лондон",
+  Китай: "Пекін",
+  Норвегия: "Осло"
+}
+
+date.each do |country, capital|
+    puts "Країна #{country}..."
+
+    user_input = gets.strip.downcase
+
+    if user_input == capital.downcase
+      puts "Відповідь вірна ;)"
+      puts "__________________________"
+    else
+      puts "Помілкова відповідь!"
+      puts "Повинно бути: #{capital}."
+      puts "__________________________"
+    end
+end
