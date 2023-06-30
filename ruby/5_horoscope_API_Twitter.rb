795b42075afa571b0cde88300fe97e1067f51aba
Alexander KRYO-dench <kryvondenchenkov@gmail.com>
2023-06-30 22:37:42 +0300

    add_comment

--- ruby/5_horoscope_API_Twitter.rb
+++ ruby/5_horoscope_API_Twitter.rb
@@ -1,12 +1,14 @@
 require 'faraday'
 
-token = 'AAAAAAAAAAAAAAAAAAAAAMGdoQEAAAAAE4q2YSA%2FhzqCTPejSNylgpp94Kk%3DQJWV57CTkEkUeQRCdudcv1fKxUksSXbzTl2MVXxnFx5PENR7Sm'
-url = 'https://api.twitter.com/2/users/1340584098075717635/tweets'
+token = 'AAAAAAAAAAAAAAAAAAAAAMGdoQEAAAAA7fRnIf%2FCGdO6jLS%2BIT3b37orqSw%3DWayTt9qY4AeK559gjF6B1ZH6l8GHl0Ryzc0trhrDhJTgFPXeVn'
+url = 'https://api.twitter.com/2/users/1672711007179796480/tweets'
 
 response = Faraday.get(
     url,
     {max_results: 12},
-    {"Authorizatoin" => "Bearer #{token}"}
+    { "Authorization" => "Bearer #{token}" }
 )
 
 puts response.body
+
+#twitter прибрав безкоштовний доступ до API, тому авторізація не проходить.
